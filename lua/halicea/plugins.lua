7b733065783e9a5c864d513f41644075cc28e137
halicea <costa@codechem.com>
2023-11-19 11:32:03 +0100

    README Update

--- lua/halicea/plugins.lua
+++ lua/halicea/plugins.lua
@@ -240,5 +240,21 @@ require('lazy').setup(
                 --   If not available, we use `mini` as the fallback
                 -- "rcarriga/nvim-notify",
             },
+            config = function()
+                require("noice").setup({
+                    lsp = {
+                        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                        override = {
+                            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                            ["vim.lsp.util.stylize_markdown"] = true,
+                            ["cmp.entry.get_documentation"] = true,
+                        },
+                    },
+                    cmdline ={
+                        enabled = true,
+                        view= "cmdline",
+                    }
+                })
+            end,
         },
     })
